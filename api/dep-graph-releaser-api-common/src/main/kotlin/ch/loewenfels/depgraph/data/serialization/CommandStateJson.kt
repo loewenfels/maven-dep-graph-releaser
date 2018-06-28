loewenfels/maven-dep-graph-releaser

commit 8ec07dbaece86ff84dcb45c8510c4f2da47e2713

rename entries of CommandStateJson.State to UPPER case

api/dep-graph-releaser-api-common/src/main/kotlin/ch/loewenfels/depgraph/data/serialization/CommandStateJson.kt
@@ -14,28 +14,28 @@ data class CommandStateJson(
     constructor(state: State, previous: CommandStateJson) : this(state, null, previous)
 
     enum class State {
-        Waiting,
-        Ready,
-        ReadyToReTrigger,
-        Queueing,
-        InProgress,
-        Succeeded,
-        Failed,
-        Deactivated,
-        Disabled
+        WAITING,
+        READY,
+        READY_TO_RE_TRIGGER,
+        QUEUEING,
+        IN_PROGRESS,
+        SUCCEEDED,
+        FAILED,
+        DEACTIVATED,
+        DISABLED
     }
 }
 
 fun fromJson(json: CommandStateJson): CommandState = when (json.state) {
-    Waiting -> CommandState.Waiting(json.dependencies ?: throwIllegal("dependencies", "Waiting"))
-    Ready -> CommandState.Ready
-    ReadyToReTrigger -> CommandState.ReadyToReTrigger
-    Queueing -> CommandState.Queueing
-    InProgress -> CommandState.InProgress
-    Succeeded -> CommandState.Succeeded
-    Failed -> CommandState.Failed
-    Deactivated -> CommandState.Deactivated(fromJson(json.previous ?: throwIllegal("previous", "Deactivated")))
-    Disabled -> CommandState.Disabled
+    WAITING -> CommandState.Waiting(json.dependencies ?: throwIllegal("dependencies", WAITING.name))
+    READY -> CommandState.Ready
+    READY_TO_RE_TRIGGER -> CommandState.ReadyToReTrigger
+    QUEUEING -> CommandState.Queueing
+    IN_PROGRESS -> CommandState.InProgress
+    SUCCEEDED -> CommandState.Succeeded
+    FAILED -> CommandState.Failed
+    DEACTIVATED -> CommandState.Deactivated(fromJson(json.previous ?: throwIllegal("previous", DEACTIVATED.name)))
+    DISABLED -> CommandState.Disabled
 }
 
 private fun throwIllegal(fieldName: String, stateName: String): Nothing = throw IllegalArgumentException("$fieldName must be defined for state $stateName")

dep-graph-releaser-gui/src/main/kotlin/ch.loewenfels.depgraph/gui/serialization/ChangeApplier.kt
@@ -165,7 +165,7 @@ object ChangeApplier {
     private fun serializeWaitingDependencies(newState: CommandState.Waiting, command: Command) {
         /* state has to be put in the following structure
             "state": {
-                "state": "Waiting",
+                "state": "WAITING",
                 "dependencies": [
                     {
                         "t": "ch.loewenfels.depgraph.data.maven.MavenProjectId",

dep-graph-releaser-serialization/src/main/kotlin/ch/loewenfels/depgraph/serialization/CommandStateAdapter.kt
@@ -13,15 +13,15 @@ object CommandStateAdapter {
 
     @ToJson
     fun toJson(state: CommandState): CommandStateJson = when (state) {
-        is CommandState.Waiting -> CommandStateJson(Waiting, state.dependencies)
-        is CommandState.Ready -> CommandStateJson(Ready)
-        is CommandState.ReadyToReTrigger -> CommandStateJson(ReadyToReTrigger)
-        is CommandState.Queueing -> CommandStateJson(Queueing)
-        is CommandState.InProgress -> CommandStateJson(InProgress)
-        is CommandState.Succeeded -> CommandStateJson(Succeeded)
-        is CommandState.Failed -> CommandStateJson(Failed)
-        is CommandState.Deactivated -> CommandStateJson(Deactivated, toJson(state.previous))
-        is CommandState.Disabled -> CommandStateJson(Disabled)
+        is CommandState.Waiting -> CommandStateJson(WAITING, state.dependencies)
+        is CommandState.Ready -> CommandStateJson(READY)
+        is CommandState.ReadyToReTrigger -> CommandStateJson(READY_TO_RE_TRIGGER)
+        is CommandState.Queueing -> CommandStateJson(QUEUEING)
+        is CommandState.InProgress -> CommandStateJson(IN_PROGRESS)
+        is CommandState.Succeeded -> CommandStateJson(SUCCEEDED)
+        is CommandState.Failed -> CommandStateJson(FAILED)
+        is CommandState.Deactivated -> CommandStateJson(DEACTIVATED, toJson(state.previous))
+        is CommandState.Disabled -> CommandStateJson(DISABLED)
     }
 
     @FromJson

dep-graph-releaser-gui/src/main/kotlin/ch.loewenfels.depgraph/gui/components/formComponents.kt
@@ -17,7 +17,7 @@ fun DIV.textFieldReadOnlyWithLabel(
     menu: Menu,
     inputAct: INPUT.() -> Unit = {}
 ) {
-    textFieldWithLabel(id, label, value, menu, { readonly = true; inputAct() })
+    textFieldWithLabel(id, label, value, menu) { readonly = true; inputAct() }
 }
 
 fun DIV.textFieldWithLabel(id: String, label: String, value: String, menu: Menu, inputAct: INPUT.() -> Unit) {

dep-graph-releaser-gui/src/main/kotlin/ch.loewenfels.depgraph/gui/serialization/deserialize.kt
@@ -132,7 +132,7 @@ private fun fakeEnumsName(json: CommandStateJson): CommandStateJson {
     while (tmp != null) {
         //necessary to fake an enum's name attribute (state is actually a json object and not really a CommandStateJson)
         js("tmp.state = {name: tmp.state}")
-        tmp = if (tmp.state.name == "Deactivated") {
+        tmp = if (tmp.state.name == CommandStateJson.State.DEACTIVATED.name) {
             tmp.previous
         } else {
             null

commit bc4fdfe90639753f32cac7145ca72c9abe0b7b97

use CommandStateJson.State in ChangeApplier instead of CommandState
This fixes a bug which was introduced with 8ec07db. Before it did not
matter that we used CommandState instead of CommandStateJson.State
because both had the same case. Now it matters and thus we have to
turn CommandState into a CommandStateJson first.

api/dep-graph-releaser-api-common/src/main/kotlin/ch/loewenfels/depgraph/data/serialization/CommandStateJson.kt
@@ -27,6 +27,19 @@ data class CommandStateJson(
     }
 }
 
+fun toJson(state: CommandState) : CommandStateJson = when (state) {
+    is CommandState.Waiting -> CommandStateJson(WAITING, state.dependencies)
+    is CommandState.Ready -> CommandStateJson(READY)
+    is CommandState.ReadyToReTrigger -> CommandStateJson(READY_TO_RE_TRIGGER)
+    is CommandState.Queueing -> CommandStateJson(QUEUEING)
+    is CommandState.RePolling -> CommandStateJson(RE_POLLING)
+    is CommandState.InProgress -> CommandStateJson(IN_PROGRESS)
+    is CommandState.Succeeded -> CommandStateJson(SUCCEEDED)
+    is CommandState.Failed -> CommandStateJson(FAILED)
+    is CommandState.Deactivated -> CommandStateJson(DEACTIVATED, toJson(state.previous))
+    is CommandState.Disabled -> CommandStateJson(DISABLED)
+}
+
 fun fromJson(json: CommandStateJson): CommandState = when (json.state) {
     WAITING -> CommandState.Waiting(json.dependencies ?: throwIllegal("dependencies", WAITING.name))
     READY -> CommandState.Ready

dep-graph-releaser-gui/src/main/kotlin/ch.loewenfels.depgraph/gui/serialization/ChangeApplier.kt
@@ -5,6 +5,7 @@ import ch.loewenfels.depgraph.data.*
 import ch.loewenfels.depgraph.data.maven.MavenProjectId
 import ch.loewenfels.depgraph.data.maven.jenkins.JenkinsCommand
 import ch.loewenfels.depgraph.data.maven.jenkins.M2ReleaseCommand
+import ch.loewenfels.depgraph.data.serialization.toJson
 import ch.loewenfels.depgraph.gui.Gui
 import ch.loewenfels.depgraph.gui.components.Pipeline
 import ch.loewenfels.depgraph.gui.elementById
@@ -146,7 +147,7 @@ object ChangeApplier {
 
         if (previousState::class != newState::class) {
             val stateObject = js("({})")
-            stateObject.state = newState::class.simpleName
+            stateObject.state = toJson(newState).state.name
             if (newState is CommandState.Deactivated) {
                 stateObject.previous = command.state
             }

dep-graph-releaser-serialization/src/main/kotlin/ch/loewenfels/depgraph/serialization/CommandStateAdapter.kt
@@ -2,7 +2,6 @@ package ch.loewenfels.depgraph.serialization
 
 import ch.loewenfels.depgraph.data.CommandState
 import ch.loewenfels.depgraph.data.serialization.CommandStateJson
-import ch.loewenfels.depgraph.data.serialization.CommandStateJson.State.*
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.ToJson
 
@@ -12,18 +11,7 @@ import com.squareup.moshi.ToJson
 object CommandStateAdapter {
 
     @ToJson
-    fun toJson(state: CommandState): CommandStateJson = when (state) {
-        is CommandState.Waiting -> CommandStateJson(WAITING, state.dependencies)
-        is CommandState.Ready -> CommandStateJson(READY)
-        is CommandState.ReadyToReTrigger -> CommandStateJson(READY_TO_RE_TRIGGER)
-        is CommandState.Queueing -> CommandStateJson(QUEUEING)
-        is CommandState.RePolling -> CommandStateJson(RE_POLLING)
-        is CommandState.InProgress -> CommandStateJson(IN_PROGRESS)
-        is CommandState.Succeeded -> CommandStateJson(SUCCEEDED)
-        is CommandState.Failed -> CommandStateJson(FAILED)
-        is CommandState.Deactivated -> CommandStateJson(DEACTIVATED, toJson(state.previous))
-        is CommandState.Disabled -> CommandStateJson(DISABLED)
-    }
+    fun toJson(state: CommandState) = ch.loewenfels.depgraph.data.serialization.toJson(state)
 
     @FromJson
     fun fromJson(json: CommandStateJson) = ch.loewenfels.depgraph.data.serialization.fromJson(json)